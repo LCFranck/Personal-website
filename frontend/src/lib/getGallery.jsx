279430a853eecee54395e21a5b1d0d7851c61b66
LCFranck <cassandra.franck@gmail.com>
2026-01-18 21:40:59 +0200

    modified to improve speed

--- frontend/src/lib/getGallery.jsx
+++ frontend/src/lib/getGallery.jsx
@@ -1,12 +1,12 @@
 import fs from "fs";
 import path from "path";
 
-//function to get all image filenames from the galleryImages folder
-export function getGalleryImages() {
-  const galleryDir = path.join(process.cwd(), "public/galleryImages");
-  const files = fs.readdirSync(galleryDir);
+const galleryDir = path.join(process.cwd(), "public/galleryImages");
 
-  return files.filter((file) => //checks the filetype of files in folder, to make sure only images are returned
-    /\.(png|jpg|jpeg|gif|webp)$/i.test(file)
-  );
+const galleryImages = fs.readdirSync(galleryDir).filter(file =>
+  /\.(png|jpg|jpeg|gif|webp)$/i.test(file)
+);
+
+export function getGalleryImages() {
+  return galleryImages;
 }
